a8bb68009a0a13dfd75e0817f1e89286788d3e2e
Diego Taveras <diegotaveras2@outlook.com>
2023-02-06 14:59:24 -0500

    adding stylesheets

--- functions.php
+++ functions.php
@@ -7,3 +7,9 @@ function fitGym_menus(){
 };
 
 add_action('init','fitGym_menus');
+
+function fitGym_scripts_styles(){
+    wp_enqueue_style('style',get_stylesheet_uri(), array(),'1.0.0');
+}
+
+add_action('wp_enqueue_scripts','fitGym_scripts_styles');
