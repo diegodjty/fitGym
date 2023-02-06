712be709ace49ee18bc67908f24f633db83518de
Diego Taveras <diegotaveras2@outlook.com>
2023-02-06 15:06:07 -0500

    adding normalize and google fonts

--- functions.php
+++ functions.php
@@ -9,7 +9,8 @@ function fitGym_menus(){
 add_action('init','fitGym_menus');
 
 function fitGym_scripts_styles(){
-    wp_enqueue_style('style',get_stylesheet_uri(), array(),'1.0.0');
+    wp_enqueue_style('normalize','https://necolas.github.io/normalize.css/8.0.1/normalize.css',array(),'8.0.1');
+    wp_enqueue_style('style',get_stylesheet_uri(), array('normalize'),'1.0.0');
 }
 
 add_action('wp_enqueue_scripts','fitGym_scripts_styles');
