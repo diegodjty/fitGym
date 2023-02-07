133099963c811e4795e50303012a8d630975395e
Diego Taveras <diegotaveras2@outlook.com>
2023-02-06 22:52:35 -0500

    refactoring header

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function fitGym_setup(){
+    // IMG
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme','fitGym_setup');
+
 function fitGym_menus(){
     register_nav_menus( array(
         'main-menu' => __('Main Menu','fitGym'),
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head() ?>
+</head>
+<body>
+    <header class="header">
+        <div class="container nav-bar">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logo">
+            </div>
+            <?php 
+                $args = array(
+                    'theme_location' => 'main-menu',
+                    'container' => 'nav',
+                    'container_class' =>'main-menu'
+                ); 
+                wp_nav_menu($args) 
+            ?>
+        </div>
+    </header>
