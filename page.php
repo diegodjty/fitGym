ac641e4039b707787f8aa6a6e6724d17ff88500a
Diego Taveras <diegotaveras2@outlook.com>
2023-02-06 22:52:15 -0500

    adding support to images

---
+++ page.php
@@ -0,0 +1,15 @@
+<?php get_header() ?>
+    <main class="container section">
+        <?php
+            while(have_posts()): the_post();
+                the_title('<h1 class="text-center primary-text">','</h1>');
+                // If it has an img add that class
+                if(has_post_thumbnail()){
+                    the_post_thumbnail('full',array('class'=>'feature-img'));
+                }
+                the_content();
+            endwhile;
+        ?>
+    </main>
+</body>
+</html>
