5ad65b5350bcb5633f7be34f3c4d9076cacd6b14
Diego Taveras <diegotaveras2@outlook.com>
2023-02-10 13:41:20 -0500

    create welcome section

--- front-page.php
+++ front-page.php
@@ -1,6 +1,13 @@
 <?php get_header() ?>
+    <section class="welcome section container text-center">
+        <h2 class="primary-text">
+            <?php the_field('heading') ?>
+        </h2>
+        <p class="">
+            <?php the_field('heading_text') ?>
+        </p>
+    </section>
     <main class="container section">
-        <h1>From home</h1>
     </main>
 <?php
     get_footer();
