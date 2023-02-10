ce731fa0b432484e10b43dee39f7fc4a5346bc94
Diego Taveras <diegotaveras2@outlook.com>
2023-02-09 22:22:16 -0500

    style to image gallery

--- page-gallery.php
+++ page-gallery.php
@@ -8,7 +8,30 @@
     <?php
         while(have_posts()): the_post();
             the_title('<h1 class="text-center primary-text">','</h1>');
-            the_content();    
+
+            // get gallery
+            $galery = get_post_gallery(get_the_ID(),false);
+
+            // Get ID's on an array
+            $galery_images_ID=explode(",",$galery['ids']);
+            ?>
+                <ul class="image-gallery">
+                    <?php 
+                        foreach($galery_images_ID as $id){
+                            $big_image = wp_get_attachment_image_src($id,'large')[0];
+                            $full_image = wp_get_attachment_image_src($id,'full')[0];
+
+                            ?>
+                                <li>
+                                    <a href="<?php echo $full_image; ?>">
+                                        <img src="<?php echo $big_image; ?>" alt="gallery image">
+                                    </a>
+                                </li>
+                            <?php
+                        }
+                    ?>
+                </ul>
+            <?php
         endwhile;
     ?>
     </main>
