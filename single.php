777333b636f6620abe5fa5bcacb59df74130d3b0
Diego Taveras <diegotaveras2@outlook.com>
2023-02-13 00:25:03 -0500

    add comment form and style if logged in

--- single.php
+++ single.php
@@ -3,7 +3,9 @@
         <?php
             get_template_part('template-parts/post')
         ?>
-       
+       <div class="comments">
+            <?php comment_form()?>
+       </div>
     </main>
 <?php
     get_footer();
